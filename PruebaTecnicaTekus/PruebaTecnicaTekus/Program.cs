0ee2c5c88aa5e524b60c5a5309f723e0e955628f
wilmar alfaro <wgalfaro2016@gmail.com>
2024-08-17 13:05:57 -0500

    adding string connection

--- PruebaTecnicaTekus/PruebaTecnicaTekus/Program.cs
+++ PruebaTecnicaTekus/PruebaTecnicaTekus/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PruebaTecnicaTekus.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +11,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<TekusContext>(
+opts => opts.UseSqlServer(
+                builder.Configuration.GetConnectionString("Tekus"),
+                options => options.EnableRetryOnFailure(maxRetryCount: 5, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null)
+            )
+);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
